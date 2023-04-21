f2d1c36e9a202e553fa4d41cd696d7a792837b0b
Skarkal <karkalsaad@gmail.com>
2023-04-21 15:08:08 +0000

    Parsing... not done yet

--- _printf.c
+++ _printf.c
@@ -10,8 +10,8 @@ int ft_printf(const char *format, ...)
 	len = 0;
 	va_start(list, format);
 	while (format[i])
-    {
-        if (format[i] == '%')
+	{
+		if (format[i] == '%')
         {
 			if (format[i + 1] == '\0')
 				break ;
@@ -25,15 +25,25 @@ int ft_printf(const char *format, ...)
 				len += ft_putnbr(va_arg(list, int));
 			else if (format[i + 1] == 'u')
 				len += ft_putnbr_unsigned(va_arg(list, unsigned int));
-			else if (format[i + 1] == 'o')
+
+			else if (format[i + 1] == 'o')                 // for oct -> Task 3
 				len += ft_putnbr_base(va_arg(list, unsigned int), B8);
+			else if (format[i + 1] == 'b')                 // for binary --> Task 2
+				len += ft_putnbr_base(va_arg(list, unsigned int), B2);
+			else if (format[i + 1] == 'S')                 // for non_printable_character --> Task 5
+				len += ft_non_printable(va_arg(list, char *));
+			else if (format[i + 1] == 'R')                 // for rot13 --> Task 14
+				len += prints_rot13(va_arg(list, char *));
+			else if (format[i + 1] == 'r')                 // for reverse String --> Task 13
+				len += ft_strrev(va_arg(list, char *));
+
 			else if (format[i + 1] == 'x')
 				len += ft_putnbr_base(va_arg(list, unsigned int), B16L);
 			else if (format[i + 1] == 'X')
 				len += ft_putnbr_base(va_arg(list, unsigned int), B16U);
 			else if (format[i + 1] == '%')
 				len += ft_putchar('%');
-			else if (format[i + 1] != 'c' || format[i + 1] != 'i' || format[i + 1] != 'd' || format[i + 1] != 's' || format[i + 1] != 'X' || format[i + 1] != 'x' || format[i + 1] != 'p' || format[i + 1] != 'o')
+			else
 			{
 				len += ft_putchar(format[i]);
 				len += ft_putchar(format[i + 1]);
@@ -49,73 +59,33 @@ int ft_printf(const char *format, ...)
 }
 
 //----------------------------------------------------//
-//                 MAIN N__1   Done ✅
-//           Crazy test hhhhhh
+//                 MAIN   Done ✅
+//             Crazy test hhhhhh
 //----------------------------------------------------//
 
-// int main()
-// {
-//     char *s = "souha";
-//     char *sa = "saad";
-//     char *add = "55";
-//     char *msg = "This is the best printf daret f 3 days i guess hahahhaha";
-//     int age = 20;
-//     int tl = 183;
-//     char m = 'M';
-//     int x = 255;
-//     unsigned int max = 4294967295;
-//
-// 	puts("---------------------------------------------------------");
-//     int our = ft_printf("Hello |%s| my name is |%s| \nIm |%dyo| and -%c- |%icm|\n i live in %%%%%% \n with Address |%p| & with this code |%x| and also |%X|\nu == |%u|\n\n%s\n\n", s, sa, age, m, tl, add, x, x, max, msg );
-//     printf("\nFull LEN Diyalna  =============>> %d\n\n", our);
-// 	puts("---------------------------------------------------------\n");
-//     int pur =   printf("Hello |%s| my name is |%s| \nIm |%dyo| and -%c- |%icm|\n i live in %%%%%% \n with Address |%p| & with this code |%x| and also |%X|\nu == |%u|\n\n%s\n\n", s, sa, age, m, tl, add, x, x, max, msg );
-//     printf("\nFull LEN Diyalhom =============>> %d\n\n", pur);
-// 	puts("---------------------------------------------------------\n");
-//
-// 	                          // Soo All Good ✔
-//
-//     return (0);
-// }
+int main()
+{
+    char s[6] = "souha";
+    char *sa = "saad";
+    char *add = "55";
+    char *msg = "This is the best printf daret f 3 days i guess hahahhaha";
+    int age = 20;
+    int tl = 183;
+    char m = 'M';
+    int x = 255;
+    unsigned int max = 4294967295;
 
-//---------------------------------------------------------------------------//
-//                 MAIN N__2   I guss all Done ✅
-//       hada l main Li kayn f ALX  ban liya mazyen same results idk checki tantii 
-//---------------------------------------------------------------------------//
+	ft_putnbr(INT_MIN);
+	puts("");
+	ft_putnbr(INT_MAX);
+	puts("");
+	puts("\n---------------------------------------------------------");
+	printf("{%o}", 41);
+	int our = ft_printf("Hello |%s|\nmy name is |%s|\nIm |%dyo|\nand -%c- |%icm|\ni live in %%%%%%\nwith Address |%p|\n& with this code |%x|\nand also |%X|\nu == |%u|\n\n%s\nrevers--> %r\nand ROT13 %R\nbin(98) == %b\nocta %o\n\n", s, sa, age, m, tl, add, x, x, max, msg, "aliahuos", "abc_nop", 98, 41 );
+	printf("\nFull LEN Diyalna  =============>> %d\n\n", our);
+	puts("---------------------------------------------------------\n");
 
-// int main()
-// {
-// 	int len;
-//     int len2;
-//     unsigned int ui;
-//     void *addr;
-//
-//     len = ft_printf("Let's try to printf a simple sentence.\n");
-//     len2 = printf("Let's try to printf a simple sentence.\n");
-//     ui = (unsigned int)INT_MAX + 1024;
-//     addr = (void *)0x7ffe637541f0;
-//     ft_printf("Length:[%d, %i]\n", len, len);
-//     printf("Length:[%d, %i]\n", len2, len2);
-//     ft_printf("Negative:[%d]\n", -762534);
-//     printf("Negative:[%d]\n", -762534);
-//     ft_printf("Unsigned:[%u]\n", ui);
-//     printf("Unsigned:[%u]\n", ui);
-//     ft_printf("Unsigned octal:[%o]\n", ui);
-//     printf("Unsigned octal:[%o]\n", ui);
-//     ft_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-//     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-//     ft_printf("Character:[%c]\n", 'H');
-//     printf("Character:[%c]\n", 'H');
-//     ft_printf("String:[%s]\n", "I am a string !");
-//     printf("String:[%s]\n", "I am a string !");
-//     ft_printf("Address:[%p]\n", addr);
-//     printf("Address:[%p]\n", addr);
-//     len = ft_printf("Percent:[%%]\n");
-//     len2 = printf("Percent:[%%]\n");
-//     ft_printf("Len:[%d]\n", len);
-//     printf("Len:[%d]\n", len2);
-//     ft_printf("Unknown:[%r]\n");
-//     printf("Unknown:[%r]\n");
-//     return (0);
-// }
+	                          // Soo All Good i guess hhhh ✔
 
+    return (0);
+}

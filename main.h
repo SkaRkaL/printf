8527b7ee892b652b75f9a337c1c0d6ee8b852a7b
Skarkal <karkalsaad@gmail.com>
2023-04-21 15:04:24 +0000

    Header file

--- main.h
+++ main.h
@@ -8,19 +8,24 @@
 # include <unistd.h>
 # include <limits.h>
 
-# define B16L "0123456789abcdef"
+# define B2   "01"
+# define B8   "01234567"
+# define B10  "0123456789"
 # define B16U "0123456789ABCDEF"
-# define B2 "01"
-# define B8 "01234567"
-# define B10 "0123456789"
+# define B16L "0123456789abcdef"
 
 
-int ft_putnbr_unsigned(unsigned int nbr);
-int	ft_putnbr_base(unsigned long long nbr, char *base);
-int	ft_putAddress(unsigned long long nbr);
-int	ft_putnbr(int nbr);
-int	ft_putstr(char *str);
-int	ft_putchar(char c);
+int		ft_non_printable(char *str); // Task 5
+int		prints_rot13(char *s); // Task 14
+int		ft_strrev(char *s);
+
+
+int		ft_putnbr_unsigned(unsigned int nbr);
+int		ft_putnbr_base(unsigned long int nbr, char *base);
+int		ft_putAddress(unsigned long long nbr);
+int		ft_putnbr(int nbr);
+int		ft_putstr(char *str);
+int		ft_putchar(char c);
 
 int ft_printf(const char *format, ...);
 

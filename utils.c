7fb7d5507f3db8b2ca329c8c1359869e49eca164
Skarkal <karkalsaad@gmail.com>
2023-04-21 15:05:14 +0000

    Task 5 and fix little problem

--- utils.c
+++ utils.c
@@ -10,8 +10,8 @@ int ft_putstr(char *str)
     int count;
 
     count = 0;
-	if (str == NULL)
-		return (ft_putstr("(null)")); // in case 3titha ft_printf("%s", NULL); should print "(null)"
+	if (str == NULL) // in case 3titha ft_printf("%s", NULL); should print "(null)"
+		return (ft_putstr("(null)"));
     if (*str)
     {
         count += ft_putchar(*str);
@@ -20,25 +20,22 @@ int ft_putstr(char *str)
     return (count);
 }
 
-int ft_putnbr(int nbr)
+int ft_putnbr(int n) // rediit putnbr diyalk lawla
 {
-    int count;
-    
-    count = 0;
-    if (nbr < 0)
+    int count = 0, nbr;
+    long i = n;
+    if (i < 0)
     {
-        nbr *= -1;
+        i *= -1;
         count += ft_putchar('-');
     }
-    if (nbr > 9)
-    {
-        count += ft_putnbr(nbr / 10);
-        count += ft_putnbr(nbr % 10);
-    }
-    else
-        count += ft_putchar(nbr + 48);
+    nbr = i % 10;
+    if (i > 9)
+        count += ft_putnbr(i / 10);
+    count += ft_putchar(nbr + 48);
     return (count);
 }
+
 
 int ft_putnbr_unsigned(unsigned int nbr)
 {
@@ -54,15 +51,15 @@ int ft_putnbr_unsigned(unsigned int nbr)
     return (count);
 }
 
-int ft_putnbr_base(unsigned long long nbr, char *base)
+int ft_putnbr_base(unsigned long int nbr, char *base)
 {
-    unsigned long long len_base;
+    unsigned long int len_base;
     int count, p;
 
     len_base = 0;
     while (base[len_base])
         len_base++;
-    count = 0;
+    count = 0; // was count++; o sa7e7tha hhh 
     if (nbr >= len_base)
         count += ft_putnbr_base(nbr / len_base, base);
     p = ft_putchar(base[nbr % len_base]);
@@ -79,3 +76,10 @@ int ft_putAddress(unsigned long long nbr)
     i += ft_putnbr_base(nbr, B16L);
     return (i);
 }
+
+
+// int main (void)
+// {
+// 	ft_putstr_non_printable("Best\nSchool\t");
+// 	ft_putchar('\n');
+// }

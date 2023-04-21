077fc6b0e0629c686cfbff7b247dbe9ff6608d72
Skarkal <karkalsaad@gmail.com>
2023-04-21 15:06:56 +0000

    adding Task 13 and 14

---
+++ task_13_14.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+int ft_strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	prints_rot13(char *str) // Task 14
+{
+	int count = 0;
+	int i = 0;
+	char *tmp;
+	if(str == NULL)
+		return (0);
+	tmp = malloc(ft_strlen(str) + 1);
+	if(tmp == NULL)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'm')
+			tmp[i] = str[i] + 13;
+		else if (str[i] >= 'n' && str[i] <= 'z')
+			tmp[i] = str[i] - 13;
+		else if (str[i] >= 'A' && str[i] <= 'M')
+			tmp[i] = str[i] + 13;
+		else if (str[i] >= 'N' && str[i] <= 'Z')
+			tmp[i] = str[i] - 13;
+		else
+			tmp[i] = str[i];
+		count += write(1, &tmp[i], 1);
+		i++;
+	}
+	tmp[i] = '\0';
+	free(tmp);
+	return (count);
+}
+
+
+int	ft_strrev(char *s) // Task 13
+{
+	int	i;
+	int	count = 0;
+	int	len;
+	char	*tmp;
+
+	if(s == NULL)
+		return(0); 
+	i = 0;
+	len = ft_strlen(s) - 1;
+	tmp = malloc(len + 1);
+	if(!tmp)
+		return(0); 
+	while (s[i])
+	{
+		tmp[i] = s[len];
+		i++;
+		len--;
+	}
+	count += ft_putstr(tmp);
+	free(tmp);
+	return (count);
+}

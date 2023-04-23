ef7c26f93df3a6127b6c602c84d529731f691ffd
souhailas142 <souhailas142@gmail.com>
2023-04-23 15:30:17 -0700

    .

--- _printf.c
+++ _printf.c
@@ -1,57 +1,72 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+int cheeck(va_list *list, char c)
 {
 	int len;
-	int i;
+
+	len = 0;
+	if (c == 'c')
+		len += _putchar(va_arg(*list, int));
+	else if (c == 's')
+		len += _putstr(va_arg(*list, char *));
+	else if (c == 'p')
+		len += _putAddress(va_arg(*list, unsigned long long));
+	else if ((c == 'd') || (c == 'i'))
+		len += _putnbr(va_arg(*list, int));
+	else if (c == 'u')
+		len += _putnbr_u(va_arg(*list, unsigned int));
+	else if (c == 'u')
+		len += _putnbr_u(va_arg(*list, unsigned int));
+	else if (c == 'o')
+		len += _putnbr_base(va_arg(*list, unsigned int), B8);
+	else if (c == 'b')
+		len += _putnbr_base(va_arg(*list, unsigned int), B2);
+	else if (c == 'S')
+		len += _non_printable(va_arg(*list, char *));
+	else if (c == 'R')
+		len += _prints_rot13(va_arg(*list, char *));
+	else if (c == 'r')
+		len += _strrev(va_arg(*list, char *));
+	else if (c == 'x')
+		len += _putnbr_base(va_arg(*list, unsigned int), B16L);
+	else if (c == 'X')
+		len += _putnbr_base(va_arg(*list, unsigned int), B16U);
+	else if (c == '%')
+		len += _putchar(c);
+	else
+	{
+		len += _putchar('%');
+		len += _putchar(c);
+	}
+	return (len);
+}
+
+int _printf(const char *format, ...)
+{
+	int len, i;
 	va_list	list;
 
-	i = 0;
+	i = -1;
 	len = 0;
 	va_start(list, format);
-	while (format[i])
+	while (format[++i])
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
 				break;
-			else if (format[i + 1] == 'c')
-				len += _putchar(va_arg(list, int));
-			else if (format[i + 1] == 's')
-				len += _putstr(va_arg(list, char *));
-			else if (format[i + 1] == 'p')
-				len += _putAddress(va_arg(list, unsigned long int));
-			else if ((format[i + 1] == 'd') || (format[i + 1] == 'i'))
-				len += _putnbr(va_arg(list, int));
-			else if (format[i + 1] == 'u')
-				len += _putnbr_u(va_arg(list, unsigned int));
-			else if (format[i + 1] == 'o')
-				len += _putnbr_base(va_arg(list, unsigned int), B8);
-			else if (format[i + 1] == 'b')
-				len += _putnbr_base(va_arg(list, unsigned int), B2);
-			else if (format[i + 1] == 'S')
-				len += _non_printable(va_arg(list, char *));
-			else if (format[i + 1] == 'R')
-				len += _prints_rot13(va_arg(list, char *));
-			else if (format[i + 1] == 'r')
-				len += _strrev(va_arg(list, char *));
-			else if (format[i + 1] == 'x')
-				len += _putnbr_base(va_arg(list, unsigned int), B16L);
-			else if (format[i + 1] == 'X')
-				len += _putnbr_base(va_arg(list, unsigned int), B16U);
-			else if (format[i + 1] == '%')
-				len += _putchar('%');
-			else
-			{
-				len += _putchar(format[i]);
-				len += _putchar(format[i + 1]);
-			}
+			len += cheeck(&list, format[i + 1]);
 			i++;
 		}
 		else
 			len += _putchar(format[i]);
-		i++;
 	}
-	va_end(list);
-	return (len);
+	return (va_end(list), len);
+}
+int main(void)
+{
+	int i = _printf("1234567890 %s\n", "True");
+	int j = printf("1234567890 %s\n", "True");
+
+	printf("\ni = %d\nj = %d\n", i, j);
 }
